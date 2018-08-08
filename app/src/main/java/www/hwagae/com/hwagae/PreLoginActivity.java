3a46cb472153ee858d38cb49a5f5349aabcf9d83
ChoiHyeji <gpwlwhddls1@naver.com>
2018-08-08 16:14:35 +0900

    2018_08_08 16:14 로그인 전 화면 만들고, 텍스트 눌러서 화면 전환 해주기까지 만들었음

--- app/src/main/java/www/hwagae/com/hwagae/PreLoginActivity.java
+++ app/src/main/java/www/hwagae/com/hwagae/PreLoginActivity.java
@@ -1,15 +1,31 @@
 package www.hwagae.com.hwagae;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PreLoginActivity extends AppCompatActivity {
+// 로그인 전 화면 : 페이스북 로그인 화면으로 넘겨주는 액티비티
 
+    TextView tvFb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login);
 
-        setTitle("");
+        setTitle("화개장터");
+
+        tvFb = findViewById(R.id.tvFb);
+
+        tvFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(PreLoginActivity.this, LoginActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
     }
 }
